2d4a5cb46b4413294e9d39d94cc6bbad1b3494a3
hyong <hyong_cs@outlook.com>
2020-12-18 00:56:41 +0800

    shiro

--- src/main/java/com/suep/sos/Config/MyWebConfigurer.java
+++ src/main/java/com/suep/sos/Config/MyWebConfigurer.java
@@ -1,17 +1,13 @@
 package com.suep.sos.Config;
 
 import com.suep.sos.Interceptor.LoginInterceptor;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class MyWebConfigurer implements WebMvcConfigurer {
